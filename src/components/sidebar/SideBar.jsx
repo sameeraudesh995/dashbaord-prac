c7ec15075cd2d0213092ceeb7ba75d33afa34ad2
Sameera Udesh <sameeraudesh75@gmail.com>
2025-06-14 12:10:30 +0530

    widget created

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -11,8 +11,11 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/DarkModeContext";
+import { useContext } from "react";
+
 const SideBar = () => {
+    const { dispatch } = useContext(DarkModeContext);
     return (
         <div className='sidebar'>
             <div className="top">
@@ -27,18 +30,18 @@ const SideBar = () => {
                         <span>Dashboard</span>
                     </li>
                     <p className="title">LISTS</p>
-                    <Link to="/users" style={{textDecoration: "none"}}>
-                        <li>
-                            <PersonOutlineIcon className="icon"/>
-                            <span>Users</span>
-                        </li>
-                    </Link>
-                    <Link to="/products" style={{textDecoration: "none"}}>
-                        <li>
-                            <StoreIcon className="icon"/>
-                            <span>Products</span>
-                        </li>
-                    </Link>
+
+                    <li>
+                        <PersonOutlineIcon className="icon"/>
+                        <span>Users</span>
+                    </li>
+
+
+                    <li>
+                        <StoreIcon className="icon"/>
+                        <span>Products</span>
+                    </li>
+
                     <li>
                         <CreditCardIcon className="icon"/>
                         <span>Orders</span>
@@ -80,7 +83,16 @@ const SideBar = () => {
                     </li>
                 </ul>
             </div>
-            <div className="bottom">color option</div>
+            <div className="bottom">
+                <div
+                    className="colorOption"
+                    onClick={() => dispatch({type: "LIGHT"})}
+                ></div>
+                <div
+                    className="colorOption"
+                    onClick={() => dispatch({type: "DARK"})}
+                ></div>
+            </div>
         </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import "./Home.scss"
 import SideBar from "../../components/sidebar/SideBar";
+import NavBar from "../../components/navbar/NavBar";
+import Widget from "../../components/widget/Widget";
 
 const Home = () => {
     return (
         <div className='home'>
-
             <SideBar/>
-            <div className='homeContainer'>Container</div>
+            <div className='homeContainer'>
+                <NavBar/>
+                <div className='widgets'>
+                    <Widget type="user"/>
+                    <Widget type="order"/>
+                    <Widget type="earning"/>
+                    <Widget type="balance"/>
+                </div>
+            </div>
         </div>
     )
 }
